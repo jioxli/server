d19e696c9d4aa90fd2d97c3ee8365eb5aca0ff43
Joshua Tenorio <joshliam09@gmail.com>
2022-10-17 00:59:38 -0700

    lists ree

--- db-setup.sql
+++ db-setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE nashorstats;
 
 CREATE TABLE league(
-    name_id CHAR(3) PRIMARY KEY,
+    name_id CHAR(3) SERIAL PRIMARY KEY,
     split CHAR(2),
     year INT(4),
     region VARCHAR(3)
@@ -9,6 +9,12 @@ CREATE TABLE league(
 );
 
 CREATE TABLE team(
-    teamid VARCHAR(3) PRIMARY KEY,
+    team_id VARCHAR(3) SERIAL PRIMARY KEY,
     name VARCHAR(255)
+);
+
+CREATE TABLE player(
+    player_id SERIAL PRIMARY KEY,
+    ign VARCHAR(50),
+    teams VARCHAR(3)[], -- TODO: is this right?
 );
