3002c56c6db11ba52db7023146b226914972f5f6
Joshua Tenorio <joshliam09@gmail.com>
2022-10-28 13:42:06 -0700

    more table code

--- db-setup.sql
+++ db-setup.sql
@@ -1,15 +1,18 @@
 CREATE DATABASE nashorstats;
 
 CREATE TABLE league(
-    name_id CHAR(3) SERIAL PRIMARY KEY,
+    name_id SERIAL PRIMARY KEY,
+    league_name VARCHAR(6),
     split CHAR(2),
-    year INT(4),
+    year INT,
     region VARCHAR(3)
     -- TODO: list of teams ? or separate table
 );
 
 CREATE TABLE team(
-    team_id VARCHAR(3) SERIAL PRIMARY KEY,
+    team_id SERIAL PRIMARY KEY,
+    team_code VARCHAR(3),
+    region VARCHAR(3),
     name VARCHAR(255)
 );
 
@@ -17,4 +20,16 @@ CREATE TABLE player(
     player_id SERIAL PRIMARY KEY,
     ign VARCHAR(50),
     teams VARCHAR(3)[], -- TODO: is this right?
+);
+
+CREATE TABLE game(
+    game_id SERIAL PRIMARY KEY,
+    team_red references team(team_id),
+    team_blue references team(team_id),
+    players_blue VARCHAR(40)[],
+    players_red VARCHAR(40)[],
+    kills_red INT,
+    kills_blue INT,
+    duration INT,
+    red_win BOOLEAN
 );
